c09612fd6a0c61549cd7bd164ae3e6ddfd21e97b
Dimitar Ivanov <>
2019-08-11 18:26:27 +0300

    Changed some variable names Changed bike.time to be to the time the bicycle is in active mode, it will go to zero when bike is stopped Fixed issue where totalKM is updated with +30km at some point
    Known bug: Speed is not calculated correct (yet to investigate this one on a real bike)

--- bikemetercode/bikemetercode.ino
+++ bikemetercode/bikemetercode.ino
@@ -1,9 +1,19 @@
 #include "SSD1306_minimal.h"
 #include "EEPROM.h"
-#include <avr/interrupt.h>
 SSD1306_Mini oled; // Declare the OLED object
 
-#define CYCLE 5
+#define F_CPU 16000000UL
+#define SECS_PER_MIN (60UL)
+#define SECS_PER_HOUR (3600UL)
+#define SECS_PER_DAY (SECS_PER_HOUR * 24L)
+#define ONE_KILOMETER 1
+
+/* Useful Macros for getting elapsed time */
+#define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)
+#define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN)
+#define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
+
+#define CYCLE 10
 #define MS_TO_S 0.001
 #define KMH_TO_MS 0.277777778
 #define RADIAN_S 0.10472
@@ -12,14 +22,14 @@ volatile bool pinChanged = false;
 byte revolutions;
 long totalKM = 0;
 unsigned int rpm;
-unsigned long passedtime;
-unsigned long totalCycleTime = 0;
+unsigned long rpmTime;
+unsigned long beginCycleTime = 0;
 int speed = 0;
 
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 int reading = LOW;
-bool updateCycleTime = false;
+bool updateCycleTime = true;
 unsigned long totalMilageUpdate = 0;
 
 // the following variables are unsigned longs because the time, measured in
@@ -30,6 +40,7 @@ byte debounceDelay = 10;    // the debounce time; increase if the output flicker
 struct bike_data {
   char current_speed[10];
   char total_kilometers[15];
+  char time[15];
 } bike;
 
 void updateSpeed() {
@@ -42,29 +53,44 @@ void updateSpeed() {
 void updateTotalKM() {
   //convert km/h to m/s
   int kmhToMs = (speed * KMH_TO_MS);
-  unsigned long timeValue = ((millis() - totalCycleTime) * MS_TO_S);
+
+  unsigned long timeValue = ((millis() - beginCycleTime) * MS_TO_S);
+
   int elapsedKilometers = kmhToMs * timeValue;
+
   int totalElapsedKilometers = (int)(elapsedKilometers * MS_TO_S);
 
+  unsigned long timeElapsedTime = ((millis() - beginCycleTime) / 1000);
+  int hours = numberOfHours(timeElapsedTime);
+  int minutes = numberOfMinutes(timeElapsedTime);
+  int seconds = numberOfSeconds(timeElapsedTime);
+
+  sprintf(bike.time, "Time: %02d:%02d:%02d", hours, minutes , seconds);
+  oled.cursorTo(0, 10);
+  oled.printString(bike.time);
+
   sprintf(bike.total_kilometers, "Total: %07d", totalKM);
-  oled.cursorTo(0, 22); // x:0, y:23
+  oled.cursorTo(0, 20); // x:0, y:23
   oled.printString(bike.total_kilometers);
 
   //we have elapsed kilometer
-  if (totalElapsedKilometers > 0) {
+  if (totalElapsedKilometers >= 0) {
     updateCycleTime = true;
-    totalKM = totalKM + totalElapsedKilometers;
+    totalKM = totalKM + ONE_KILOMETER;
     EEPROMWritelong(0, totalKM);
   }
 }
 
 void setup() {
+  cli();
   oled.init(0x3C); // Initializes the display to the specified address
   oled.clear(); // Clears the display
   GIMSK = 0b00100000;    // turns on pin change interrupts
   PCMSK = 0b00010000;    // turn on interrupts on pins PB0, PB1, PB4
   //EEPROMWritelong(0, 0);
   totalKM = EEPROMReadlong(0);
+  updateSpeed();
+  updateTotalKM();
   sei();                 // enables interrupts
 }
 
@@ -103,10 +129,8 @@ void handlePinChange() {
 
 unsigned long test = 0;
 
-void loop() {
-  //handlePinChange();
-
-  if (millis() - test > 100) {
+void testSpeed() {
+  if (millis() - test > 500) {
     revolutions = CYCLE;
     if (reading == LOW) {
       reading = HIGH;
@@ -115,6 +139,12 @@ void loop() {
     }
     test = millis();
   }
+}
+
+void loop() {
+  handlePinChange();
+
+  //testSpeed();
 
   // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
@@ -125,7 +155,7 @@ void loop() {
   //if there is no cycling activity for more than 1.5S, assume that the bike is stopped
   if ((millis() - lastDebounceTime) > 1500) {
     rpm = 0;
-    passedtime = 0;
+    rpmTime = 0;
     updateCycleTime = true;
     revolutions = 0;
     updateSpeed();
@@ -138,16 +168,14 @@ void loop() {
 
     if (buttonState == HIGH) {
       if (revolutions >= CYCLE) {
-        cli();
-        rpm = 60000 / (millis() - passedtime) * CYCLE;
+        rpm = 60000 / (millis() - rpmTime) * CYCLE;
         if (updateCycleTime) {
-          totalCycleTime = millis();
+          beginCycleTime = millis();
           updateCycleTime = false;
         }
-        passedtime = millis();
+        rpmTime = millis();
         updateSpeed();
         revolutions = 0;
-        sei();
       }
     }
   }
