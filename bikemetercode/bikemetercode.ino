a833e41a15040e6846fe4543a2e07ac595193e7a
Dimitar Ivanov <>
2019-08-12 08:42:45 +0300

    variable rename minor code change

--- bikemetercode/bikemetercode.ino
+++ bikemetercode/bikemetercode.ino
@@ -7,34 +7,38 @@ SSD1306_Mini oled; // Declare the OLED object
 #define SECS_PER_HOUR (3600UL)
 #define SECS_PER_DAY (SECS_PER_HOUR * 24L)
 #define ONE_KILOMETER 1
+#define TOTAL_KM_EEPROM_ADDRESS 0
 
 /* Useful Macros for getting elapsed time */
 #define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)
 #define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN)
 #define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
-
+#define LCD_I2C_ADDRESS 0x3C
 #define CYCLE 10
 #define MS_TO_S 0.001
 #define KMH_TO_MS 0.277777778
 #define RADIAN_S 0.10472
 #define TYRE_DIAMETER_M 0.6604 //(in meters) 26inch standart tyre
+
+//time storing variables
+unsigned long rpmTime;
+unsigned long beginOneKmTime = 0;
+unsigned long totalMilageUpdate = 0;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+
+
 volatile bool pinChanged = false;
+byte speed = 0;
 byte revolutions;
 long totalKM = 0;
 unsigned int rpm;
-unsigned long rpmTime;
-unsigned long beginCycleTime = 0;
-int speed = 0;
-
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
+int switchState;             // the current reading from the input pin
+int lastSwitchState = LOW;   // the previous reading from the input pin
 int reading = LOW;
-bool updateCycleTime = true;
-unsigned long totalMilageUpdate = 0;
+bool updateOneKmTime = true;
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 byte debounceDelay = 10;    // the debounce time; increase if the output flickers
 
 struct bike_data {
@@ -54,13 +58,13 @@ void updateTotalKM() {
   //convert km/h to m/s
   int kmhToMs = (speed * KMH_TO_MS);
 
-  unsigned long timeValue = ((millis() - beginCycleTime) * MS_TO_S);
+  unsigned long timeValue = ((millis() - beginOneKmTime) * MS_TO_S);
 
   int elapsedKilometers = kmhToMs * timeValue;
 
   int totalElapsedKilometers = (int)(elapsedKilometers * MS_TO_S);
 
-  unsigned long timeElapsedTime = ((millis() - beginCycleTime) / 1000);
+  unsigned long timeElapsedTime = millis() / 1000;
   int hours = numberOfHours(timeElapsedTime);
   int minutes = numberOfMinutes(timeElapsedTime);
   int seconds = numberOfSeconds(timeElapsedTime);
@@ -75,44 +79,50 @@ void updateTotalKM() {
 
   //we have elapsed kilometer
   if (totalElapsedKilometers >= 0) {
-    updateCycleTime = true;
+    updateOneKmTime = true;
     totalKM = totalKM + ONE_KILOMETER;
-    EEPROMWritelong(0, totalKM);
+    EEPROMWriteTotalKM(totalKM);
   }
 }
 
 void setup() {
   cli();
-  oled.init(0x3C); // Initializes the display to the specified address
+  oled.init(LCD_I2C_ADDRESS); // Initializes the display to the specified address
   oled.clear(); // Clears the display
   GIMSK = 0b00100000;    // turns on pin change interrupts
   PCMSK = 0b00010000;    // turn on interrupts on pins PB0, PB1, PB4
-  //EEPROMWritelong(0, 0);
-  totalKM = EEPROMReadlong(0);
+
+  totalKM = EEPROMReadTotalKM();
   updateSpeed();
   updateTotalKM();
   sei();                 // enables interrupts
 }
 
-long EEPROMReadlong(long address) {
-  long four = EEPROM.read(address);
-  long three = EEPROM.read(address + 1);
-  long two = EEPROM.read(address + 2);
-  long one = EEPROM.read(address + 3);
+long EEPROMReadTotalKM() {
+  long four = EEPROM.read(TOTAL_KM_EEPROM_ADDRESS);
+  long three = EEPROM.read(TOTAL_KM_EEPROM_ADDRESS + 1);
+  long two = EEPROM.read(TOTAL_KM_EEPROM_ADDRESS + 2);
+  long one = EEPROM.read(TOTAL_KM_EEPROM_ADDRESS + 3);
 
   return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
 }
 
-void EEPROMWritelong(int address, long value) {
-  byte four = (value & 0xFF);
-  byte three = ((value >> 8) & 0xFF);
-  byte two = ((value >> 16) & 0xFF);
-  byte one = ((value >> 24) & 0xFF);
+void EEPROMWriteTotalKM(long value) {
 
-  EEPROM.write(address, four);
-  EEPROM.write(address + 1, three);
-  EEPROM.write(address + 2, two);
-  EEPROM.write(address + 3, one);
+  for (byte i = 0; i < 4; i++) {
+    byte toStoreInEeprom = ((value >> (i * 8)) & 0xFF);
+    EEPROM.write(TOTAL_KM_EEPROM_ADDRESS + i, toStoreInEeprom);
+  }
+
+  //    byte four = (value & 0xFF);
+  //    byte three = ((value >> 8) & 0xFF);
+  //    byte two = ((value >> 16) & 0xFF);
+  //    byte one = ((value >> 24) & 0xFF);
+  //
+  //    EEPROM.write(TOTAL_KM_EEPROM_ADDRESS, four);
+  //    EEPROM.write(TOTAL_KM_EEPROM_ADDRESS + 1, three);
+  //    EEPROM.write(TOTAL_KM_EEPROM_ADDRESS + 2, two);
+  //    EEPROM.write(TOTAL_KM_EEPROM_ADDRESS + 3, one);
 }
 
 void handlePinChange() {
@@ -128,7 +138,6 @@ void handlePinChange() {
 }
 
 unsigned long test = 0;
-
 void testSpeed() {
   if (millis() - test > 500) {
     revolutions = CYCLE;
@@ -147,7 +156,7 @@ void loop() {
   //testSpeed();
 
   // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
+  if (reading != lastSwitchState) {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
@@ -156,22 +165,22 @@ void loop() {
   if ((millis() - lastDebounceTime) > 1500) {
     rpm = 0;
     rpmTime = 0;
-    updateCycleTime = true;
+    updateOneKmTime = true;
     revolutions = 0;
     updateSpeed();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
+    if (reading != switchState) {
+      switchState = reading;
     }
 
-    if (buttonState == HIGH) {
+    if (switchState == HIGH) {
       if (revolutions >= CYCLE) {
         rpm = 60000 / (millis() - rpmTime) * CYCLE;
-        if (updateCycleTime) {
-          beginCycleTime = millis();
-          updateCycleTime = false;
+        if (updateOneKmTime) {
+          beginOneKmTime = millis();
+          updateOneKmTime = false;
         }
         rpmTime = millis();
         updateSpeed();
@@ -180,7 +189,7 @@ void loop() {
     }
   }
 
-  lastButtonState = reading;
+  lastSwitchState = reading;
 
   if (millis() - totalMilageUpdate > 999) {
     totalMilageUpdate = millis();
